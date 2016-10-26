8c901231f436967f8fd3bd07b559fc4cc6607552
Leipo Yan <leipo.yan@u-blox.com>
2016-10-25 19:28:48 -0700

    WIP.

--- main.cpp
+++ main.cpp
@@ -16,50 +16,45 @@
  */
 
 #include "mbed.h"
-//#include "TCPSocket.h"
-//#include "net/eth/EthernetInterface/EthernetInterface.h"
+#include "C027_Support/SerialPipe.h"
 
 
 DigitalOut led1(LED1);
-//EthernetInterface eth;
-//TCPSocket socket;
+
+SerialPipe mdm(MDMTXD,MDMRXD);
+SerialPipe pc(USBTX, USBRX);
 
 int main() {
+    char cmd[10];
+    char buf[10];
 
-    //eth.connect();
-    //const char *ip = eth.get_ip_address();
-    //const char *mac = eth.get_mac_address();
-
-    //printf("IP address is: %s\r\n", ip ? ip : "No IP");
-    //printf("MAC address is: %s\r\n", mac ? mac : "No MAC");
-
-    /*
-    socket.open(&eth);
-    socket.connect("developer.mbed.org", 80);
-
-    // Send a simple http request
-    char sbuffer[] = "GET / HTTP/1.1\r\nHost: developer.mbed.org\r\n\r\n";
-    int scount = socket.send(sbuffer, sizeof sbuffer);
-    printf("sent %d [%.*s]\r\n", scount, strstr(sbuffer, "\r\n")-sbuffer, sbuffer);
-
-    // Recieve a simple http response and print out the response line
-    char rbuffer[64];
-    int rcount = socket.recv(rbuffer, sizeof rbuffer);
-    printf("recv %d [%.*s]\r\n", rcount, strstr(rbuffer, "\r\n")-rbuffer, rbuffer);
-
-    // Close the socket to return its memory and bring down the network interface
-    socket.close();
-    */
-
-
-    //eth.disconnect();
     printf("Done\r\n");
     
-    //BlinkyLoop
+    mdm.baud(MDMBAUD);
+    pc.baud(MDMBAUD);
+
     while (true) {
         led1 = !led1;
-	printf("test...\n");
+#if 0
+        if (pc.readable() && mdm.writeable()) {
+            mdm.putc(pc.getc());
+        }
+        if (mdm.readable() && pc.writeable()) {
+            pc.putc(mdm.getc());
+        }
+#else
+	memset(cmd, 0, sizeof(cmd));
+	memset(buf, 0, sizeof(buf));
+
+	sprintf(cmd, "AT+GMM\n");
+        mdm.put(cmd, strlen(cmd), 1);
+	mdm.get(buf, sizeof(buf), 0);
+ 	printf("mdm Buf is: %s.\n", strlen(buf) ? "NULL" : buf);
+
         Thread::wait(500);
+
+#endif
     }
 }
+
 
